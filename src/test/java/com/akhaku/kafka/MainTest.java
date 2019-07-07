c3ddf79fd9adabf9f733be906593633b6524bf12
Ammar Khaku <akhaku@netflix.com>
2019-07-06 18:20:20 -0700

    Add hello world

---
+++ src/test/java/com/akhaku/kafka/MainTest.java
@@ -0,0 +1,13 @@
+package com.akhaku.kafka;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        Main main = new Main();
+        assertNotNull(main);
+    }
+}
