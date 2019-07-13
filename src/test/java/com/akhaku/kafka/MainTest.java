3ee301fdb1471fbb9d5857e5cb49bcc04c15cb4b
Ammar Khaku <akhaku@netflix.com>
2019-07-13 14:03:49 -0700

    Produce messages with callback

--- src/test/java/com/akhaku/kafka/MainTest.java
+++ src/test/java/com/akhaku/kafka/MainTest.java
@@ -2,16 +2,37 @@ package com.akhaku.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainTest {
     private static final String TOPIC = "first_topic";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainTest.class);
+
     @Test
     public void testRunProducer() {
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "hello world");
         KafkaProducer<String, String> producer = new KafkaProducerFactory().create();
-        producer.send(record);
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, "hello world " + i);
+            producer.send(record, (recordMetadata, e) -> {
+                if (e == null) {
+                    LOGGER.info("Received new metadata: {}", toString(recordMetadata));
+                } else {
+                    LOGGER.error("Error producing message", e);
+                }
+            });
+        }
         producer.flush();
         producer.close();
+    }
+
+    private static String toString(RecordMetadata recordMetadata) {
+        return new StringBuilder("RecordMetadata{topic=").append(recordMetadata.topic())
+            .append(",parition=").append(recordMetadata.partition())
+            .append(",offset=").append(recordMetadata.offset())
+            .append(",timestamp=").append(recordMetadata.timestamp())
+            .append("}").toString();
     }
 }
